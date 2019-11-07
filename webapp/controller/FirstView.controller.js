a8583d3723e2c6f8b4f4c8227d5501f0443a2c50
Swapnil Palande <swap13nil@gmail.com>
2019-11-07 06:46:40 +0000

    Status color

--- webapp/controller/FirstView.controller.js
+++ webapp/controller/FirstView.controller.js
@@ -11,6 +11,18 @@ sap.ui.define([
 		onInit: function () {
       var oModel = new JSONModel("model/products.json");
 			this.getView().setModel(oModel); 
-		}
+		},
+		onAfterRendering:function(){
+	
+		this.getView().byId("idProductsTable").getItems().forEach(function(item,index){
+		
+				if(item.getCells()[3].getText() === "Available"){
+					item.getCells()[3].addStyleClass("green");
+				}
+			//	else{
+			//			item.getCells()[3].addStyleClass("red");
+					
+			//	}
+			}); }
 	});
 });
